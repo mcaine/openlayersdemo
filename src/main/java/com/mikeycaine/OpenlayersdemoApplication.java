e864ba34f2c311cdfbbe7e491352e855ca5baeea
Mike Caine <Mike.Caine@geoplace.co.uk>
2017-03-27 09:50:40 +0100

    initial commit

---
+++ src/main/java/com/mikeycaine/OpenlayersdemoApplication.java
@@ -0,0 +1,12 @@
+package com.mikeycaine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlayersdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenlayersdemoApplication.class, args);
+	}
+}
